405252e708b993e3e50c1395d082e266a8add4a7
feud72 <feud72@gmail.com>
2019-09-19 14:41:59 +0900

    #11

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,19 @@
-let movies = [
-  {
-    id: 0,
-    name: 'Star Wars - The new one',
-    score: 1,
-  },
-  {
-    id: 1,
-    name: 'Avengers - The new one',
-    score: 8,
-  },
-  {
-    id: 2,
-    name: 'The Godfather I',
-    score: 99,
-  },
-  {
-    id: 3,
-    name: 'Logan',
-    score: 2,
-  },
-];
+import axios from 'axios';
 
-export const getMovies = () => movies;
+const api = axios.create({
+  baseURL: 'https://yts-proxy.now.sh/list_movies.json',
+});
 
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === Number(id));
-  return filteredMovies[0];
-};
-
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== Number(id));
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+export const getMovies = async (limit = 20, rating = 0) => {
+  const {
+    data: {
+      data: {movies},
+    },
+  } = await axios.get('https://yts-proxy.now.sh/list_movies.json', {
+    params: {
+      limit: limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
 };
