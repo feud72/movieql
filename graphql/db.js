dc3f6a4295872f596ce078661ad24586ed46e705
feud72 <feud72@gmail.com>
2019-09-19 16:09:06 +0900

    #13, end

--- graphql/db.js
+++ graphql/db.js
@@ -1,16 +1,44 @@
 import axios from 'axios';
 
-const baseURL = 'https://yts-proxy.now.sh/list_movies.json';
+const api = axios.create({
+  baseURL: 'https://yts-proxy.now.sh/',
+});
 
 export const getMovies = async (limit = 20, rating = 0) => {
   const {
     data: {
       data: {movies},
     },
-  } = await axios.get(baseURL, {
+  } = await api.get('list_movies.json', {
     params: {
       limit: limit,
       minimum_rating: rating,
+    },
+  });
+  return movies;
+};
+
+export const getDetail = async id => {
+  const {
+    data: {
+      data: {movie},
+    },
+  } = await api.get('movie_details.json', {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
+};
+
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: {movies},
+    },
+  } = await api.get('movie_suggestions.json', {
+    params: {
+      movie_id: id,
     },
   });
   return movies;
